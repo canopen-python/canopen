03cee34184f0a66378d0597463048b5a763ddc47
André Colomb <src@andre.colomb.de>
2025-06-15 22:20:59 +0200

    Really unspecified size.

--- test/test_sdo.py
+++ test/test_sdo.py
@@ -99,7 +99,7 @@ class TestSDO(unittest.TestCase):
         # Same with padding to a full SDO frame
         self.data = [
             (TX, b'\x40\x00\x14\x02\x00\x00\x00\x00'),
-            (RX, b'\x4f\x00\x14\x02\xfe\x00\x00\x00')
+            (RX, b'\x42\x00\x14\x02\xfe\x00\x00\x00')
         ]
         trans_type = self.network[2].sdo[0x1400]['Transmission type RPDO 1'].raw
         self.assertEqual(trans_type, 254)
